4c60c39faeb4797abf504ee19e8d9458baffec72
Boris Bogdanov Simeonov <twistbobg@gmail.com>
2015-03-16 22:32:28 +0200

    First commit

---
+++ RandomCominations/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace RandomCominations
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.mainLabel = new System.Windows.Forms.Label();
+            this.tbOne = new System.Windows.Forms.TextBox();
+            this.tbTwo = new System.Windows.Forms.TextBox();
+            this.tbThree = new System.Windows.Forms.TextBox();
+            this.tbFour = new System.Windows.Forms.TextBox();
+            this.tbFive = new System.Windows.Forms.TextBox();
+            this.tbSix = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // mainLabel
+            // 
+            this.mainLabel.AutoSize = true;
+            this.mainLabel.BackColor = System.Drawing.Color.Black;
+            this.mainLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.mainLabel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.mainLabel.Font = new System.Drawing.Font("Times New Roman", 24F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.mainLabel.ForeColor = System.Drawing.Color.Orange;
+            this.mainLabel.Location = new System.Drawing.Point(96, 9);
+            this.mainLabel.Name = "mainLabel";
+            this.mainLabel.Size = new System.Drawing.Size(497, 38);
+            this.mainLabel.TabIndex = 0;
+            this.mainLabel.Text = "Get Your Winning Combination Now";
+            this.mainLabel.MouseClick += new System.Windows.Forms.MouseEventHandler(this.mainLabel_MouseClick);
+            this.mainLabel.MouseEnter += new System.EventHandler(this.mainLabel_MouseEnter);
+            this.mainLabel.MouseLeave += new System.EventHandler(this.mainLabel_MouseLeave);
+            // 
+            // tbOne
+            // 
+            this.tbOne.BackColor = System.Drawing.SystemColors.MenuText;
+            this.tbOne.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.tbOne.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbOne.ForeColor = System.Drawing.Color.Orange;
+            this.tbOne.Location = new System.Drawing.Point(49, 174);
+            this.tbOne.MaxLength = 2;
+            this.tbOne.Name = "tbOne";
+            this.tbOne.ReadOnly = true;
+            this.tbOne.Size = new System.Drawing.Size(61, 44);
+            this.tbOne.TabIndex = 1;
+            this.tbOne.TabStop = false;
+            this.tbOne.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.tbOne.WordWrap = false;
+            // 
+            // tbTwo
+            // 
+            this.tbTwo.BackColor = System.Drawing.SystemColors.MenuText;
+            this.tbTwo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.tbTwo.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbTwo.ForeColor = System.Drawing.Color.Orange;
+            this.tbTwo.Location = new System.Drawing.Point(152, 174);
+            this.tbTwo.MaxLength = 2;
+            this.tbTwo.Name = "tbTwo";
+            this.tbTwo.ReadOnly = true;
+            this.tbTwo.Size = new System.Drawing.Size(61, 44);
+            this.tbTwo.TabIndex = 2;
+            this.tbTwo.TabStop = false;
+            this.tbTwo.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.tbTwo.WordWrap = false;
+            // 
+            // tbThree
+            // 
+            this.tbThree.BackColor = System.Drawing.SystemColors.MenuText;
+            this.tbThree.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.tbThree.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbThree.ForeColor = System.Drawing.Color.Orange;
+            this.tbThree.Location = new System.Drawing.Point(255, 174);
+            this.tbThree.MaxLength = 2;
+            this.tbThree.Name = "tbThree";
+            this.tbThree.ReadOnly = true;
+            this.tbThree.Size = new System.Drawing.Size(61, 44);
+            this.tbThree.TabIndex = 3;
+            this.tbThree.TabStop = false;
+            this.tbThree.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.tbThree.WordWrap = false;
+            // 
+            // tbFour
+            // 
+            this.tbFour.BackColor = System.Drawing.SystemColors.MenuText;
+            this.tbFour.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.tbFour.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbFour.ForeColor = System.Drawing.Color.Orange;
+            this.tbFour.Location = new System.Drawing.Point(358, 174);
+            this.tbFour.MaxLength = 2;
+            this.tbFour.Name = "tbFour";
+            this.tbFour.ReadOnly = true;
+            this.tbFour.Size = new System.Drawing.Size(61, 44);
+            this.tbFour.TabIndex = 4;
+            this.tbFour.TabStop = false;
+            this.tbFour.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.tbFour.WordWrap = false;
+            // 
+            // tbFive
+            // 
+            this.tbFive.BackColor = System.Drawing.SystemColors.MenuText;
+            this.tbFive.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.tbFive.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbFive.ForeColor = System.Drawing.Color.Orange;
+            this.tbFive.Location = new System.Drawing.Point(461, 174);
+            this.tbFive.MaxLength = 2;
+            this.tbFive.Name = "tbFive";
+            this.tbFive.ReadOnly = true;
+            this.tbFive.Size = new System.Drawing.Size(61, 44);
+            this.tbFive.TabIndex = 5;
+            this.tbFive.TabStop = false;
+            this.tbFive.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.tbFive.WordWrap = false;
+            // 
+            // tbSix
+            // 
+            this.tbSix.BackColor = System.Drawing.SystemColors.MenuText;
+            this.tbSix.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.tbSix.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbSix.ForeColor = System.Drawing.Color.Orange;
+            this.tbSix.Location = new System.Drawing.Point(564, 174);
+            this.tbSix.MaxLength = 2;
+            this.tbSix.Name = "tbSix";
+            this.tbSix.ReadOnly = true;
+            this.tbSix.Size = new System.Drawing.Size(61, 44);
+            this.tbSix.TabIndex = 6;
+            this.tbSix.TabStop = false;
+            this.tbSix.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.tbSix.WordWrap = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(694, 412);
+            this.Controls.Add(this.tbSix);
+            this.Controls.Add(this.tbFive);
+            this.Controls.Add(this.tbFour);
+            this.Controls.Add(this.tbThree);
+            this.Controls.Add(this.tbTwo);
+            this.Controls.Add(this.tbOne);
+            this.Controls.Add(this.mainLabel);
+            this.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.MaximumSize = new System.Drawing.Size(710, 450);
+            this.MinimumSize = new System.Drawing.Size(710, 450);
+            this.Name = "Form1";
+            this.Text = "Winning Combinations";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label mainLabel;
+        private System.Windows.Forms.TextBox tbTwo;
+        private System.Windows.Forms.TextBox tbThree;
+        private System.Windows.Forms.TextBox tbFour;
+        private System.Windows.Forms.TextBox tbFive;
+        private System.Windows.Forms.TextBox tbSix;
+        private System.Windows.Forms.TextBox tbOne;
+    }
+}
+
---
+++ RandomCominations/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RandomCominations
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void mainLabel_MouseEnter(object sender, EventArgs e)
+        {
+            mainLabel.BackColor = System.Drawing.Color.Orange;
+            mainLabel.ForeColor = System.Drawing.Color.Black;
+            
+        }
+
+        private void mainLabel_MouseLeave(object sender, EventArgs e)
+        {
+            mainLabel.BackColor = System.Drawing.Color.Black;
+            mainLabel.ForeColor = System.Drawing.Color.Orange;
+        }
+
+        private void mainLabel_MouseClick(object sender, MouseEventArgs e)
+        {
+            TextBox[] textBoxArray = { tbOne, tbTwo, tbThree, tbFour, tbFive, tbSix };
+            Random rnd = new Random();
+            List<int> winningNums = new List<int>();
+            int currentNum;
+            int index = 0;
+            for (int cnt = 0; cnt <= 5; cnt++ )
+            {
+                currentNum = rnd.Next(1, 48);
+                if (!winningNums.Contains(currentNum))
+                {
+                    textBoxArray[index].Text = currentNum.ToString();
+                    winningNums.Add(currentNum);
+                    index++;
+                }
+                else
+                {
+                    cnt--;
+                }
+            }
+        }
+    }
+}
